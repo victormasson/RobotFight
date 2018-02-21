7b27f8b06afbdcfc613f3431797023eb78958b13
victormasson <victor.massonv@gmail.com>
2018-02-21 19:06:40 +0100

    .

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const http = require('http').Server(express);
+const io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+var app = express();
+//app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css/'));
+app.use('/asset', express.static(__dirname + '/img/'));
+app.listen(3000);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on('connection', function(socket) {
+    console.log('a user connected');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+// http.listen(3000, function(){
+//     console.log('listening on *:3000');
+// });
