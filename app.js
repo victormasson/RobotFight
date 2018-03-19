6f078227815677d5b5586ac602d4252308e02002
Alexis MASSEY <alexis.massey@diiage.org>
2018-03-19 20:57:04 +0100

    Business fire ball

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ app.get('/', function(req, res) {
 //     console.log('a user connected');
 // });
 
-var placeBonome = { left : 0, top : 0 };
+var placeBonome = { left : 40, top : 40 };
 var placeWindows = { right : 0, down : 0 };
 var element = { x : 0, y : 0, x1 : 0, y1 : 0};
-var placeFireBall = { left : 50, top : 0 };
+var placeFireBall = { left : 0, top : 0 };
 var placeWindows = { right : 0, down : 0 };
 
 //Création écouteur sockets
@@ -29,15 +29,17 @@ io.on('connection', function(socket){
         placeWindows.right = msg.wRight;
         placeWindows.down = msg.wDown;
 
-        placeBonome.left = 0;
-        placeBonome.top = 0;
+        placeBonome.left = 40;
+        placeBonome.top = 40;
+
+        socket.emit('btnPlay', placeBonome);
+        socket.broadcast.emit('btnPlay', placeBonome);
     });
 
     socket.on('btnLeft', function(){
         //console.log('btnLeft');
         if (placeBonome.left > 0) {
             placeBonome.left -= 20;
-            placeFireBall.left -= 20;
             //console.log('btnLeft: ' + placeBonome.left);
         
         }
@@ -48,7 +50,6 @@ io.on('connection', function(socket){
     socket.on('btnRight', function(){
         if (placeBonome.left + 200 < placeWindows.right) {
             placeBonome.left += 20;
-            placeFireBall.left += 20;
             // console.log('btnRight: ' + placeBonome.left);
         }
         socket.emit('btnRight', placeBonome);
@@ -59,7 +60,6 @@ io.on('connection', function(socket){
         // console.log('btnTop');
         if (placeBonome.top - 25 > 0) {
             placeBonome.top -= 20;
-            placeFireBall.top -= 20;
             // console.log('btnTop: ' + placeBonome.top);
         }
         socket.emit('btnTop', placeBonome);
@@ -70,20 +70,17 @@ io.on('connection', function(socket){
         // console.log('btnDown');
         if (placeBonome.top + 350 < placeWindows.down) {
             placeBonome.top += 20;
-            placeFireBall.top += 20;
             // console.log('btnDown: ' + placeBonome.top);
         }
         socket.emit('btnDown', placeBonome);
         socket.broadcast.emit('btnDown', placeBonome);
     });
 
-    socket.on('btnSpace', function(){
-        console.log('btnSpace');
-        if (placeFireBall.top > 0) {    
-            placeFireBall.top -= 20;
-            socket.emit('btnSpace', placeFireBall);
-            socket.broadcast.emit('btnSpace', placeFireBall);
-        }
+    socket.on('btnSpace', function(){ 
+        placeFireBall.top = placeBonome.top;
+        placeFireBall.left = placeBonome.left + 50
+        socket.emit('btnSpace', placeFireBall);
+        socket.broadcast.emit('btnSpace', placeFireBall);
     });
 });
 
